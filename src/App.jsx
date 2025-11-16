3d7212454810b9160e748de455dad69b9992d7fa
forger6969 <saidazim186@gmail.com>
2025-11-16 20:20:18 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Question from './Components/Question'
+
+const App = () => {
+  return (
+    <>
+
+      <div>
+
+        <Question />
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Question.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Question = () => {
+  return (
+    <div>
+
+      <div className='bg-white rounded-[10px]'>
+
+        <p>Зачем нужен useEffect?</p>
+
+        <div>
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Question
